e613b83ebdb1f88d5b71c82a2a1c4ee5875aba49
ehzawad <ehzawad@gmail.com>
2017-07-01 02:54:06 +0600

    address and indirectional value

--- dataStructure/pointer_like_c.go
+++ dataStructure/pointer_like_c.go
@@ -6,8 +6,9 @@ func main() {
 	fmt.Println("You can't alter an existing pointer like you can in C or C++")
 
 	var a int = 5
-	var aPonter *int = a
+	var aPonter *int = &a
 
 	fmt.Println("value: ", a)
 	fmt.Println("address: ", aPonter)
+	fmt.Println("addressed value: ", *aPonter)
 }
