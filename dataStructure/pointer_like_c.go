ecf5694c5899a4f7aaa2f892ace11b2f8d59c311
ehzawad <ehzawad@gmail.com>
2017-07-01 02:52:26 +0600

    Pointer :3

---
+++ dataStructure/pointer_like_c.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("You can't alter an existing pointer like you can in C or C++")
+
+	var a int = 5
+	var aPonter *int = a
+
+	fmt.Println("value: ", a)
+	fmt.Println("address: ", aPonter)
+}
