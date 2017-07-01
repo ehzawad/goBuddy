3ef0354b425ae1c5c72a9b5affebc9cf21aa57ae
ehzawad <ehzawad@gmail.com>
2017-07-01 22:36:33 +0600

    interface is cool

---
+++ interfaces/interfaceIdea.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+import "math"
+
+type Geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (rectangle Rectangle) area() float64 {
+	return rectangle.length * rectangle.width
+}
+
+func (rectangle Rectangle) perimeter() float64 {
+	return 2 * (rectangle.length + rectangle.width)
+}
+
+func (circle Circle) area() float64 {
+	return math.Pi * circle.radius * circle.radius
+}
+
+func (circle Circle) perimeter() float64 {
+	return 2 * math.Pi * circle.radius
+}
+
+func measure(obj Geometry) {
+	fmt.Println(obj)
+	fmt.Println(obj.area())
+	fmt.Println(obj.perimeter())
+}
+
+func main() {
+	r := Rectangle{length: 4, width: 3}
+	c := Circle{radius: 5}
+
+	measure(r)
+	measure(c)
+}
