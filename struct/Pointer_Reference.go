d7ed58477d7f3dac9adfd4512000212f02c36bab
ehzawad <ehzawad@gmail.com>
2017-07-01 16:25:05 +0600

    recheck pointer

--- struct/Pointer_Reference.go
+++ struct/Pointer_Reference.go
@@ -2,19 +2,32 @@ package main
 
 import "fmt"
 
-type Minutes int
+// type Minutes int
+//
+// func (m *Minutes) Increment() {
+// 	*m = (*m + 1) % 60
+// }
+//
+// func main() {
+// 	minutes := Minutes(58)
+//
+// 	for i := 1; i <= 3; i++ {
+// 		// (&minutes).Increment()
+// 		// auto pointer type detection
+// 		minutes.Increment()
+// 		fmt.Println(minutes)
+// 	}
+// }
+//
 
-func (m *Minutes) Increment() {
-	*m = (*m + 1) % 60
+type Hours int
+
+func (h *Hours) Increment() Hours {
+	*h = (*h + 1) % 24
+	return *h
 }
-
 func main() {
-	minutes := Minutes(58)
-
-	for i := 1; i <= 3; i++ {
-		// (&minutes).Increment()
-		// auto pointer type detection
-		minutes.Increment()
-		fmt.Println(minutes)
-	}
+	hours := Hours(23)
+	hours.Increment()
+	fmt.Println(hours)
 }
